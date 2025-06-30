420cf09f4821560762efe9b4e9f102a2543e522d
behruz-dev <xoliqbediyevbehruz@gmail.com>
2025-06-30 13:23:24 +0500

    fux

--- config.py
+++ config.py
@@ -8,6 +8,6 @@ load_dotenv()
 data = request.get_telegram_ids()
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
-CHANNEL_ID = utils.get_group_id_by_name(data, "Buyuk zamon channel 3")
+CHANNEL_ID = "-1002587826683"
 FIRST_CHANNEL_ID = utils.get_group_id_by_name(data, "Buyuk zamon channel") 
 SECOND_CHANNEL_ID = utils.get_group_id_by_name(data, "buyuk zamon channel 2") 
